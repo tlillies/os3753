1556e38823fd80d281c2897c4230368b8ad541a0
tom <thli2089@colorado.edu>
2015-11-06 15:44:25 -0700

    build testscript

--- Assignment4/pi-sched.c
+++ Assignment4/pi-sched.c
@@ -20,11 +20,11 @@
 #define DEFAULT_ITERATIONS 1000000
 #define RADIUS (RAND_MAX / 2)
 
-inline double dist(double x0, double y0, double x1, double y1){
+double dist(double x0, double y0, double x1, double y1){
     return sqrt(pow((x1-x0),2) + pow((y1-y0),2));
 }
 
-inline double zeroDist(double x, double y){
+double zeroDist(double x, double y){
     return dist(0, 0, x, y);
 }
 
